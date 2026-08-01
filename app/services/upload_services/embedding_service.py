505c0ec8b105dc006151ea41a2f460b7cd3a3cad
huyhd2334 <huyqame1356@gmail.com>
2026-08-01 18:55:56 +0700

    gemini-embedding-2

--- app/services/upload_services/embedding_service.py
+++ app/services/upload_services/embedding_service.py
@@ -1,31 +1,20 @@
 import asyncio
-# from sentence_transformers import SentenceTransformer
-
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# async def embed(text: str):
-
-#     loop = asyncio.get_running_loop()
-    
-#     vector = await loop.run_in_executor(
-#         None,                
-#         model.encode,         
-#         text                  
-#     )
-    
-#     return vector.tolist()
-
-from google import genai
 import os
+from google import genai
 
 client = genai.Client(
     api_key=os.getenv("GOOGLE_API_KEY")
 )
 
 async def embed(text: str):
-    response = client.models.embed_content(
-        model="text-embedding-004",
-        contents=text,
+    loop = asyncio.get_running_loop()
+
+    response = await loop.run_in_executor(
+        None,
+        lambda: client.models.embed_content(
+            model="gemini-embedding-2",
+            contents=text,
+        )
     )
 
     return response.embeddings[0].values
