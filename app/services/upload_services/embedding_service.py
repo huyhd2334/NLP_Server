edc5598038914c8ae42d22c90be179d457786841
huyhd2334 <huyqame1356@gmail.com>
2026-08-01 18:21:25 +0700

    embedding for deploye

--- app/services/upload_services/embedding_service.py
+++ app/services/upload_services/embedding_service.py
@@ -1,16 +1,31 @@
 import asyncio
-from sentence_transformers import SentenceTransformer
+# from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# async def embed(text: str):
+
+#     loop = asyncio.get_running_loop()
+    
+#     vector = await loop.run_in_executor(
+#         None,                
+#         model.encode,         
+#         text                  
+#     )
+    
+#     return vector.tolist()
+
+from google import genai
+import os
+
+client = genai.Client(
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
 
 async def embed(text: str):
-
-    loop = asyncio.get_running_loop()
-    
-    vector = await loop.run_in_executor(
-        None,                
-        model.encode,         
-        text                  
+    response = client.models.embed_content(
+        model="text-embedding-004",
+        contents=text,
     )
-    
-    return vector.tolist()
+
+    return response.embeddings[0].values
